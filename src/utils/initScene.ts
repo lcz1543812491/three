97e6424d06d6ecdd410c73de456ca2ab4032bef4
lcz <1543812491@qq.com>
2023-07-16 14:53:07 +0800

    add gsap

--- src/utils/initScene.ts
+++ src/utils/initScene.ts
@@ -1,45 +1,77 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from "three";
+import gsap from "gsap";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
+export function initScene() {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
 
-export function initScene(){
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  // cube.position.set(5,0, 0);
+  // cube.scale.set(3,2,1);
+  cube.rotation.set(Math.PI / 4, 0, 0);
 
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    const cube = new THREE.Mesh( geometry, material );
-    // cube.position.set(5,0, 0);
-    // cube.scale.set(3,2,1);
-    cube.rotation.set(Math.PI/4, 0, 0);
+  const axesHelper = new THREE.AxesHelper(5);
+  scene.add(axesHelper);
+  scene.add(cube);
+  camera.position.z = 5;
 
-    const axesHelper = new THREE.AxesHelper( 5 );
-    scene.add( axesHelper );
-    scene.add( cube );
-    camera.position.z = 5;
-    
-    const renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setSize( window.innerWidth, window.innerHeight );
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-    renderer.render( scene, camera );
-    const root = document.getElementById('root');
+  renderer.render(scene, camera);
+  const root = document.getElementById("root");
 
-    if(root){
-        root.appendChild( renderer.domElement );
+  if (root) {
+    root.appendChild(renderer.domElement);
+  }
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  //   const clock = new THREE.Clock();
+  const animate = gsap.to(cube.position, {
+    x: 5,
+    duration: 5,
+    ease: "back.out(1.7)",
+    repeat: 3,
+    yoyo: true,
+    onComplete: () => {
+      console.log("onComplete");
+    },
+  });
+  gsap.to(cube.rotation, { x: 2 * Math.PI, duration: 5, ease: "power3.out" });
+
+  window.addEventListener("dblclick", () => {
+    if (animate.isActive()) {
+      animate.pause();
+    } else {
+      animate.resume();
     }
+  });
 
-    const controls = new OrbitControls( camera, renderer.domElement );
+  function render() {
+    // console.log('@@@', time);
 
-    function render(){
-       cube.position.x += 0.01; 
-       cube.rotation.x += 0.01;
-       if(cube.position.x > 5){
-        cube.position.x = 0;
-       }
-       renderer.render(scene, camera)
-       requestAnimationFrame(render);
-    }
+    // const time = clock.getElapsedTime();
+    // const deltaTime = clock.getDelta();
+    // console.log('time', time);
+    // console.log('delta', deltaTime);
 
-    render();
+    // cube.position.x += 0.01;
+    // cube.rotation.x += 0.01;
+    // if (cube.position.x > 5) {
+    //   cube.position.x = 0;
+    // }
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  }
 
+  render();
 }
