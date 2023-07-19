c5b53a8b6e970a1292ad6995e58c83338413ff00
lcz <1543812491@qq.com>
2023-07-19 17:43:38 +0800

    add hdr

--- src/utils/initSphere.ts
+++ src/utils/initSphere.ts
@@ -2,10 +2,15 @@ import * as THREE from "three";
 import gsap from "gsap";
 import * as dat from "dat.gui";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { RGBELoader } from "three/addons/loaders/RGBELoader.js"
 import textureUrl from "../assets/WechatIMG10.jpg";
-import textureUrl1 from "../assets/WechatIMG3.jpg";
+import textureUrl1 from "../assets/WechatIMG2_1.jpg";
+import rgbeUrl from '../assets/env.hdr';
 
-console.log('textureUrl', textureUrl, typeof textureUrl);
+// console.log('textureUrl', textureUrl, typeof textureUrl);
+
+console.log('RGBELoader', RGBELoader);
+console.log('rgbeUrl', rgbeUrl);
 
 export function initScene() {
   const scene = new THREE.Scene();
@@ -34,14 +39,23 @@ export function initScene() {
   };
 
   const textureLoader = new THREE.CubeTextureLoader();
+  const rgbeLoader = new RGBELoader();
+  console.log('rgbeLoader', rgbeLoader);
+
+  rgbeLoader.load(rgbeUrl, (value) => {
+    // console.log('@@@', value);
+    value.mapping = THREE.EquirectangularReflectionMapping;
+    scene.background = value;
+  });
+
 
   const texture = textureLoader.load([
     textureUrl,
+    textureUrl1,
     textureUrl,
+    textureUrl1,
     textureUrl,
-    textureUrl,
-    textureUrl,
-    textureUrl,
+    textureUrl1,
   ]);
 
   const geometry = new THREE.SphereGeometry(2, 100, 100);
@@ -51,6 +65,9 @@ export function initScene() {
     envMap: texture,
   });
   const sphere = new THREE.Mesh(geometry, material);
+
+  // scene.background = texture;
+
   scene.add(sphere);
 
   const light = new THREE.AmbientLight(0x404040, 0.5); // soft white light
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// const demo = defineConfig({
+//   plugins: [react()],
+// })
+
+// console.log('defineConfig', demo);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.jpg', '**/*.hdr'],
 })
