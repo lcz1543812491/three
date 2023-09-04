1b804212a7a881c176036e6946bf12fddf388afb
lcz <1543812491@qq.com>
2023-09-04 17:18:34 +0800

    add buufer

--- src/threeDemo/part-2.ts
+++ src/threeDemo/part-2.ts
@@ -5,16 +5,41 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 export function part1() {
   const scene = new THREE.Scene()
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1)
-  const material = new THREE.MeshBasicMaterial({ color: '#f00' })
+  const geometry = new THREE.BoxGeometry(1, 1, 1, 20, 20, 2)
+  const material = new THREE.MeshBasicMaterial({ color: '#f00', wireframe: true })
 
   const cube = new THREE.Mesh(geometry, material)
+
+  
+
+  const geometry1 = new THREE.BufferGeometry();
+
+  const vertices = new Float32Array( [
+    -1.0, -1.0,  1.0, // v0
+     1.0, -1.0,  1.0, // v1
+     1.0,  1.0,  1.0, // v2
+    -1.0,  1.0,  1.0, // v3
+  ] );
+  
+  const indices = [
+    0, 1, 2,
+    2, 3, 0,
+  ];
+  
+  geometry.setIndex( indices );
+  geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
+  
+  const material1 = new THREE.MeshBasicMaterial( { color: 0xfff000 } );
+  const mesh = new THREE.Mesh( geometry, material1 );
+
+  scene.add(mesh)
+
 
   // cube.position.set(1, -0.5, 1)
   // cube.scale.set(2, 0.5, 0.5)
 
   // cube.rotation.y = 1.7
-  scene.add(cube)
+  // scene.add(cube)
 
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight)
   camera.position.z = 3
@@ -37,6 +62,12 @@ export function part1() {
 
   const controls = new OrbitControls(camera, render.domElement)
   controls.enableDamping = true;
+
+  window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    render.setSize(window.innerWidth, window.innerHeight);
+  });
 
   function tick() {
     // const currentTime = Date.now()
